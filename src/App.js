e7dcdda33eef61b4be78c9be161e5c2fe6154b75
anisuzzaman1 <anisuzzaman2112@gmail.com>
2022-02-17 15:58:52 +0600

    Upload Init files

--- src/App.js
+++ src/App.js
@@ -2,9 +2,30 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const number = 5555;
+  const singer = { name: 'Dr. Mahfuz', job: 'Singer' }
+  const singer2 = { name: 'Eva Rahman', job: 'Kokil Konthi' }
+
+  const singerStyle = {
+    backgroundColor: 'red',
+    color: 'white',
+    borderRadius: '15px',
+    padding: '15px'
+  }
+
   return (
     <div className="App">
       <header className="App-header">
+        <h3> Yo Yo react Mama !!!</h3>
+        <p>Happy React</p>
+        <div className='container'>
+          <h3>
+            This is my Inside Header
+          </h3>
+          <p>My Number is: {number}</p>
+          <p>Name: {singer.name} and Profession: {singer.job}</p>
+          <p style={singerStyle}>Real Singer: {singer2.name} and Original: {singer2.job}</p>
+        </div>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
